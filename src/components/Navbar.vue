1fe60ecf095a3c484b4b84aabdfe2e6124b09515
jb <dev.jbcu10@gmail.com>
2024-07-10 23:26:09 +0200

    fix sidebar btns

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div class=" px-4 flex space-y-4  bg-base-200">
     <div class="navbar">
       <div class="flex-1">
-        <label for="my-drawer-2" class="btn btn-square btn-ghost drawer-button" @click="hideDrawer">
+        <label for="my-drawer-2" class="btn btn-square drawer-button lg:hidden" >
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
             class="inline-block h-5 w-5 stroke-current">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
@@ -37,7 +37,10 @@
 </template>
 <script setup lang="ts">
 const hideDrawer = () => {
-  
+  const drawer = document.getElementById('my-drawer-2') as HTMLInputElement;
+  if (drawer) {
+    drawer.checked = false;
+  }
 };
 </script>
 
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,28 +1,63 @@
 <template>
-  <div class=" flex flex-row justify-between ">
+  <div class=" flex flex-row justify-between z-50 ">
     <div class="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
       <div class="drawer-side">
         <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
         <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
           <!-- Sidebar content here -->
+          <button class="btn btn-ghost bg-base-300  btn-circle z-50 top-0 right-0 mt-4 mr-2 absolute lg:hidden"
+            @click="close"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" aria-hidden="true" class="h-5 inline-block w-5">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+            </svg></button>
+          <li class="mb-2 font-semibold text-2xl">
+            <p>Strand</p>
+          </li>
 
-          <li class="mb-2 font-semibold text-2xl"><p>Strand</p> </li>
+          <RouterLink to="/">
+            <li>
+              <div>
+                <ChartBarIcon class="size-4" /> Dashboard
+              </div>
+            </li>
+          </RouterLink>
 
-          <RouterLink to="/"><li> <div><ChartBarIcon class="size-4" /> Dashboard</div></li></RouterLink>
-
-          <RouterLink to="/users"><li> <div><UserIcon class="size-4" /> Users</div></li></RouterLink>
-          <RouterLink to="/todo"><li><div><PencilIcon class="size-4" />Todo</div></li></RouterLink>
-          <RouterLink to="/settings"><li><div><CogIcon class="size-4" />Utilities</div></li></RouterLink>
+          <RouterLink to="/users">
+            <li>
+              <div>
+                <UserIcon class="size-4" /> Users
+              </div>
+            </li>
+          </RouterLink>
+          <RouterLink to="/todo">
+            <li>
+              <div>
+                <PencilIcon class="size-4" />Todo
+              </div>
+            </li>
+          </RouterLink>
+          <RouterLink to="/settings">
+            <li>
+              <div>
+                <CogIcon class="size-4" />Utilities
+              </div>
+            </li>
+          </RouterLink>
         </ul>
       </div>
     </div>
   </div>
 </template>
 
-<script  setup lang="ts">
-import { UserIcon, PencilIcon,CogIcon,ChartBarIcon } from '@heroicons/vue/24/outline'
- 
+<script setup lang="ts">
+import { UserIcon, PencilIcon, CogIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+const close = () => {
+  const drawer = document.getElementById('my-drawer-2') as HTMLInputElement;
+  if (drawer) {
+    drawer.checked = false;
+  }
+}
 </script>
 
 <style scoped>
